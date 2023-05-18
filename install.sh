c16d074abf8f1ab2c92521f992c09e6b0b843400
xf0r3m <jakubstasinski@protonmail.com>
2023-05-18 18:20:40 +0200

    Nowy branch - d13

--- install.sh
+++ install.sh
@@ -2,18 +2,26 @@
 
 set -e
 
-sudo apt install -y ranger git qmmp vlc lynx tmux pass qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager
+apt install -y ranger git qmmp vlc lynx tmux pass qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager conky picom vim-gtk3 thunderbird;
 
-cp -rvv home/xf0r3m/.cache /etc/skel;
+mkdir /etc/skel/.config;
+cp -rvv xfce4 /etc/skel/.config;
 
-cp -rvv home/xf0r3m/.config /etc/skel;
+mkdir /etc/skel/.config/autostart;
+cp conky.desktop /etc/skel/.config/autostart;
 
-cp home/xf0r3m/.vimrc /etc/skel;
+cp vimrc /etc/skel/.vimrc;
+cp conkyrc /etc/skel/.conkyrc;
 
-sudo rm -rf /etc/lightdm;
-sudo cp -rvv etc/lightdm /etc;
+tar -xzvf fonts.tgz -C /etc/skel;
+
+rm -rf /etc/lightdm;
+cp -rvv lightdm /etc;
 
 rm /usr/share/images/desktop-base/default;
-cp -rvv home/xf0r3m/Obrazy/* /usr/share/images/desktop-base;
-ln -s /usr/share/images/desktop-base/Volna4k-Edit.png /usr/share/images/desktop-base/default;
+cp -rvv images/* /usr/share/images/desktop-base;
+ln -s /usr/share/images/desktop-base/d13_wallpaper.png /usr/share/images/desktop-base/default;
+ln -s /usr/share/images/desktop-base/immudex_xfce_greeter_logo.png /etc/skel/.face;
 
+cp icons/* /usr/share/icons;
+ln -s /usr/share/icons/changes-prevent.png /usr/share/icons/padlock-icon.png;
